9f870af0067d908fadbd38f22695976b8f9fa027
obliviouX <xanderg@icloud.com>
2025-04-06 13:12:59 -0500

    add user offset functionality

--- Left Wrist/main.py
+++ Left Wrist/main.py
@@ -12,7 +12,7 @@ import time
 import ujson as json
 
 orange_led()
-time.sleep(0.2)
+time.sleep(1)
 
 # turn off green LED on Pico
 onboard_LED = Pin("LED", Pin.OUT)
@@ -21,7 +21,7 @@ onboard_LED.off()
 # setup calibration button
 c_button = Pin(10, Pin.IN, Pin.PULL_UP)
 c_state = c_button.value()
-#print(c_state)
+print(c_state)
 
 # set up imu power pins
 arm_imu_power = Pin(21, Pin.OUT)
@@ -30,28 +30,25 @@ arm_imu_power.on()
 wrist_imu_power = Pin(20, Pin.OUT)
 wrist_imu_power.on()
 
+time.sleep(1)
+
 # imu i2c setup
 i2c_hand = I2C(0, sda=Pin(16), scl=Pin(17), freq=400000) # define i2c pins
 i2c_arm = I2C(1, sda=Pin(18), scl=Pin(19), freq=400000)
 
 time.sleep(1)
-led_off()
 
-#hand_imu = MPU9250(i2c_hand)             # define the address of the hand imu
-#arm_imu = MPU9250(i2c_arm)               # define the address of the arm imu
 #e
 # calibration setup
 # checks if button is being held down on startup to initiate calibration
-if c_state == False:
+if c_state == 0:
     hand_dummy = MPU9250(i2c_hand)
     arm_dummy = MPU9250(i2c_arm)
     ak8963_hand = AK8963(i2c_hand)
     ak8963_arm = AK8963(i2c_arm)
     
-    hand_offset, hand_scale = ak8963_hand.calibrate(count=256, delay=200)
-    blue_led()
-    time.sleep(1)
-    arm_offset, arm_scale = ak8963_arm.calibrate(count=256, delay=200)
+    hand_offset, hand_scale = ak8963_hand.calibrate(count=10)
+    arm_offset, arm_scale = ak8963_arm.calibrate(count=10)
     
     jsonData = {"hand_offset_x": hand_offset[0], "hand_offset_y": hand_offset[1], "hand_offset_z": hand_offset[2],
                 "hand_scale_x": hand_scale[0], "hand_scale_y": hand_scale[1], "hand_scale_z": hand_scale[2],
@@ -92,24 +89,79 @@ else:
             arm_scale_y = data["arm_scale_y"]
             arm_scale_z = data["arm_scale_z"]
         
-            #print("DONE OPENING SAVEDATA")
+            print("OPENED SAVEDATA")
             hand_dummy = MPU9250(i2c_hand)  # dummy to open up access to the ak8963
             arm_dummy = MPU9250(i2c_arm)
         
-            #print("DUMMY IMUs CREATED")
+            print("DUMMY IMUs CREATED")
             ak8963_hand = AK8963(i2c_hand, offset=(hand_offset_x, hand_offset_y, hand_offset_z), scale=(hand_scale_x, hand_scale_y, hand_scale_z))
             ak8963_arm = AK8963(i2c_arm, offset=(arm_offset_x, arm_offset_y, arm_offset_z), scale=(arm_scale_x, arm_scale_y, arm_scale_z))
         
-            #print("SETTING UP REAL IMUs")
+            print("SETTING UP REAL IMUs")
             hand_imu = MPU9250(i2c_hand,ak8963=ak8963_hand)
             arm_imu = MPU9250(i2c_arm, ak8963=ak8963_arm)
-            #print("IMUs ARE SET UP")
+            print("IMUs ARE SET UP")
         
     except:
-        #print("ERROR IN SAVEDATA")
+        print("ERROR IN SAVEDATA")
         red_led()
 
-led_off()
+
+user_radial_offset = 0
+user_flexion_offset = 0
+set_offset_flag = 0
+
+def calc_wrist_angles():
+    global user_radial_offset, user_flexion_offset
+    global filtered_hand_mx, filtered_hand_my, filtered_arm_mx, filtered_arm_my
+    # roll (flexion/extension), yaw (radial)
+    
+    hand_ax = hand_imu.acceleration[0]
+    hand_ay = hand_imu.acceleration[1]
+    hand_az = hand_imu.acceleration[2]
+
+    hand_mx, hand_my, hand_mz = hand_imu.magnetic
+
+    arm_ax = arm_imu.acceleration[0]
+    arm_ay = arm_imu.acceleration[1]
+    arm_az = arm_imu.acceleration[2]
+
+    arm_mx, arm_my, arm_mz = arm_imu.magnetic
+    
+    hand_pitch = math.atan2(hand_az, hand_ax) * 180 / math.pi
+    hand_roll = math.atan2(hand_az, hand_ay) * 180 / (math.pi);
+    filtered_hand_mx = low_pass_filter(hand_mx, filtered_hand_mx)
+    filtered_hand_my = low_pass_filter(hand_my, filtered_hand_my)
+    hand_yaw =  90 - math.atan2(filtered_hand_my, filtered_hand_mx) * 180 / math.pi
+    
+    arm_pitch = math.atan2(arm_az, arm_ax) * 180 / math.pi
+    arm_roll = math.atan2(arm_az, arm_ay) * 180 / math.pi
+    arm_yaw = math.atan2(arm_my, arm_mx) * 180 / math.pi
+    filtered_arm_mx = low_pass_filter(arm_mx, filtered_arm_mx)
+    filtered_arm_my = low_pass_filter(arm_my, filtered_arm_my)
+    arm_yaw = 90 - math.atan2(filtered_arm_my, filtered_arm_mx) * 180 / math.pi
+
+    # angles in relation to arm IMU
+    roll_diff = abs(abs(round(arm_roll - hand_roll)) - user_flexion_offset)
+    pitch_diff = abs(round(arm_pitch - hand_pitch))
+    yaw_diff = abs(abs(round(((arm_yaw - hand_yaw) + 180) % 360 - 180)) - user_radial_offset)
+
+    if (roll_diff > 35 or roll_diff < -35) or (yaw_diff > 35 or yaw_diff < -35):
+        red_led()
+    else:
+        green_led()
+
+    return roll_diff, pitch_diff, yaw_diff
+
+
+# set up c button interrupt after calibration so the user can set an offset (roll and yaw)
+def user_offset_button(c_button):
+    global set_offset_flag
+    print("OFFSET BUTTON PRESSED")
+    set_offset_flag = 1
+c_button.irq(trigger=c_button.IRQ_RISING, handler=user_offset_button)
+
+
 # MAIN PROGRAM
 
 # Define UUIDs for the service and characteristic
@@ -134,19 +186,8 @@ def encode_message(message):
 
 
 async def get_imu_data():
-    hand_ax = hand_imu.acceleration[0]
-    hand_ay = hand_imu.acceleration[1]
-    hand_az = hand_imu.acceleration[2]
-
-    hand_mx, hand_my, hand_mz = hand_imu.magnetic
-
-    arm_ax = arm_imu.acceleration[0]
-    arm_ay = arm_imu.acceleration[1]
-    arm_az = arm_imu.acceleration[2]
-
-    arm_mx, arm_my, arm_mz = arm_imu.magnetic
-
-    roll_diff, pitch_diff, yaw_diff = calc_wrist_angles(hand_ax, hand_ay, hand_az, arm_ax, arm_ay, arm_az, hand_mx, hand_my, hand_mz, arm_mx, arm_my, arm_mz)
+    
+    roll_diff, pitch_diff, yaw_diff = calc_wrist_angles()
     
     # Data structure: message = f"device,roll,pitch,yaw"
     message = f"1,{roll_diff},{pitch_diff},{yaw_diff}"
@@ -164,10 +205,11 @@ async def send_data_task(connection, characteristic):
         return
 
     message = await get_imu_data()
-    #print(f"sending message: {message}")
+    print(f"sending message: {message}")
 
     try:
         msg = encode_message(message)
+        await asyncio.sleep(0.5)
         characteristic.write(msg)
         await asyncio.sleep(0.5)
 
@@ -176,6 +218,7 @@ async def send_data_task(connection, characteristic):
 
 
 async def run_peripheral_mode():
+    global user_radial_offset, user_flexion_offset, set_offset_flag
     # Set up the Bluetooth service and characteristic
     ble_service = aioble.Service(BLE_SVC_UUID)
     characteristic = aioble.Characteristic(
@@ -189,7 +232,7 @@ async def run_peripheral_mode():
     aioble.register_services(ble_service)
 
     # start to advertise BLE
-    #print(f"{BLE_NAME} starting to advertise", end="\n")
+    print(f"{BLE_NAME} starting to advertise", end="\n")
 
     while True:
         async with await aioble.advertise(
@@ -197,29 +240,36 @@ async def run_peripheral_mode():
             name=BLE_NAME,
             services=[BLE_SVC_UUID],
             appearance=BLE_APPEARANCE) as connection:
-            #print(f"{BLE_NAME} connected to another device: {connection.device}")
+            print(f"{BLE_NAME} connected to another device: {connection.device}")
 
             tasks = [
                 asyncio.create_task(send_data_task(connection, characteristic)),
             ]
             await asyncio.gather(*tasks)
             connection.disconnect()
-            #print("Disconnected from the central device.")
-            #print("Sleeping for 4 seconds.\n")
-            await asyncio.sleep(0.5)
-            led_off()
+            print("Disconnected from the central device.")
+            print("Sleeping for 4 seconds.\n")
             machine.lightsleep(4000)
-            
-    #print(f"{IAM} disconnected, waiting before advertising again...")
+
+        if set_offset_flag == 1:
+            break
 
 
 async def main():
+    global user_flexion_offset, user_radial_offset, set_offset_flag
+    rgb_led(0, 0, 20000)
     while True:
         # create a BLE task to run asynchronously
+        #UNCOMMENT BELOW THIS, COMMENTED OUT FOR TESTING
         tasks = [
             asyncio.create_task(run_peripheral_mode()), # run the run_peripheral_mode() function asynchronously
-        ]
+        ]    
         await asyncio.gather(*tasks)
+
+        if set_offset_flag == 1:
+            print("setting new offset")
+            user_flexion_offset, pitch_unused, user_radial_offset = calc_wrist_angles()
+            set_offset_flag = 0
 
 asyncio.run(main())
 
